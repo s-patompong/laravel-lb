647f4cb67e9de17082a53ffce85bf349e7a40a73
Patompong Savaengsuk <pond@netearth.net>
2016-08-27 15:55:24 +0700

    Make LogicBoxesReseller class looks more OOP.

--- examples/reseller_login.php
+++ examples/reseller_login.php
@@ -4,21 +4,21 @@ require __DIR__.'/autoload.php';
 
 use LaravelLb\LogicBoxesReseller;
 
-$lb = new LogicBoxesReseller;
-
 // Setup user id and api key
 $userId = getenv('LB_AUTH_USERID');
 $apiKey = getenv('LB_API_KEY');
 
 // Setup reseller username and password
+$resellerId = getenv('RESELLER_ID');
 $resellerUserName = getenv('RESELLER_USERNAME');
 $resellerPassword = getenv('RESELLER_PASSWORD');
 
-$lb->setUserId($userId)->setApiKey($apiKey);
+$reseller = new LogicBoxesReseller($resellerId, $resellerUserName, $resellerPassword);
 
-$response = $lb->login($resellerUserName, $resellerPassword);
+/** No need to set user id if you're using Laravel, it will automatically get the credential from config/logicboxes.php */
+$reseller->setUserId($userId)->setApiKey($apiKey);
 
-if($response)
+if($reseller->login())
 {
 	echo "Login success\n";
 }
--- src/LogicBoxesReseller.php
+++ src/LogicBoxesReseller.php
@@ -6,22 +6,31 @@ use LaravelLb\LogicBoxes;
 
 class LogicBoxesReseller extends LogicBoxes {
 
-	public function __construct()
+
+    private $resellerId = "";
+    private $userName = "";
+    private $password = "";
+
+	public function __construct($resellerId, $userName = "", $password = "")
     {
         parent::__construct();
+
         $this->resource = "resellers";
+        $this->resellerId = $resellerId;
+        $this->userName = $userName;
+        $this->password = $password;
     }
 
     /**
      * Authenticate reseller
      * @return Boolean login status
      */
-    public function login($username, $password)
+    public function login()
     {
         $method = 'authenticate';
         $variables = [
-            "username" => $username,
-            "passwd" => $password,
+            "username" => $this->userName,
+            "passwd" => $this->password,
         ];
 
         $response = $this->get($this->resource, $method, $variables)->toArray();
@@ -37,6 +46,17 @@ class LogicBoxesReseller extends LogicBoxes {
      * @param [type] $params [description]
      */
     public function addDebitNote($variables)
+    {
+        // Todo: Add debit note method goes here
+    }
+
+    /**
+     * Adds a Debit Note against the specified Sub-Reseller's Account.
+     * http://manage.netearthone.com/kb/answer/1167
+     *         
+     * @param [type] $params [description]
+     */
+    public function availableBalance()
     {
         // Todo: Add debit note method goes here
     }
