9407764260f98256b9cddf3c86e938d33d1de5cc
Patompong Savaengsuk <pond@netearth.net>
2016-08-27 11:08:38 +0700

    Add more unit test for Logicboxes core.

--- tests/LogicBoxesCoreTest.php
+++ tests/LogicBoxesCoreTest.php
@@ -2,7 +2,8 @@
 
 use PHPUnit\Framework\TestCase;
 use LaravelLb\LogicBoxes;
-use LaravelLb\InvalidFormatException;
+use LaravelLb\Exceptions\InvalidFormatException;
+use LaravelLb\Exceptions\InvalidRequestTypeException;
 
 class LogicBoxesCoreTest extends TestCase
 {
@@ -115,5 +116,136 @@ class LogicBoxesCoreTest extends TestCase
     	
     	$this->assertEquals($this->lb->getCredentialQueryString(), $credentialQueryString);
     }
+
+    /**
+     * @test
+     */
+    public function it_should_be_able_to_set_test_mode()
+    {
+        $testMode = false;
+
+        $lb = new LogicBoxes;
+        $lb->setTestMode($testMode);
+        
+        $this->assertEquals($lb->getTestMode(), $testMode);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_generate_correct_root_path_when_test_mode_is_false()
+    {
+        $testMode = false;
+        $rootPath = "https://httpapi.com/api";
+
+        $lb = new LogicBoxes;
+        $lb->setTestMode($testMode);
+        
+        $this->assertEquals($lb->getRootPath(), $rootPath);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_generate_correct_root_path_when_test_mode_is_true()
+    {
+        $testMode = true;
+        $rootPath = "https://test.httpapi.com/api";
+
+        $lb = new LogicBoxes;
+        
+        $this->assertEquals($lb->getRootPath(), $rootPath);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_be_able_to_set_method()
+    {
+        $method = "search";
+
+        $this->lb->setMethod($method);
+        
+        $this->assertEquals($this->lb->getMethod(), $method);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_be_able_to_set_resource()
+    {
+        $resource = "resellers";
+
+        $this->lb->setResource($resource);
+        
+        $this->assertEquals($this->lb->getResource(), $resource);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_has_correct_end_point()
+    {
+        $testMode = false;
+        $userId = "432256";
+        $apiKey = "SgVhO5SxAwEtF";
+        $resource = "billing";
+        $method = "reseller-balance";
+        $format = "xml";
+        $variables = [
+            "reseller-id" => "123456",
+        ];
+
+        $expectedEndpoint = "https://httpapi.com/api/billing/reseller-balance.xml?auth-userid=432256&api-key=SgVhO5SxAwEtF&reseller-id=123456";
+
+        $lb = new LogicBoxes;
+        $actualEndpoint = $lb->setTestMode($testMode)
+                             ->setUserId($userId)
+                             ->setApiKey($apiKey)
+                             ->setResource($resource)
+                             ->setMethod($method)
+                             ->setFormat($format)
+                             ->setVariables($variables)
+                             ->getEndPoint();
+        
+        $this->assertEquals($actualEndpoint, $expectedEndpoint);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_have_default_request_type_as_get()
+    {
+        $requestType = "GET";
+
+        $lb = new LogicBoxes;
+        
+        $this->assertEquals($lb->getRequestType(), $requestType);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_be_able_to_set_request_type()
+    {
+        $requestType = "POST";
+
+        $this->lb->setRequestType($requestType);
+        
+        $this->assertEquals($this->lb->getRequestType(), $requestType);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_be_able_to_throw_invalid_request_type()
+    {
+        $requestType = "PUT";
+
+        $this->expectException(InvalidRequestTypeException::class);
+
+        $this->lb->setRequestType($requestType);
+    }
+
 }
 ?>
