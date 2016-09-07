7776c6e4c83f61529b80e0a0a6bff51562655147
Patompong Savaengsuk <pond@netearth.net>
2016-09-07 15:56:45 +0700

    Add signup customer example. Add LogicboxesCustomer Class.

---
+++ examples/customer_signup.php
@@ -0,0 +1,33 @@
+<?php
+
+require __DIR__.'/autoload.php';
+
+use LaravelLb\LogicBoxesCustomer;
+
+// Setup user id and api key
+$userId = getenv('LB_AUTH_USERID');
+$apiKey = getenv('LB_API_KEY');
+
+$customer = new LogicBoxesCustomer();
+
+$customerDetail = [
+	"username" => "test_lb_customer1@netearth.net",
+	"passwd" => "qwerty123456",
+	"name" => "Netearth Test1",
+	"company" => "IDC (Thailand)",
+	"address-line-1" => "123 Thailand",
+	"city" => "Bangkok",
+	"state" => LogicBoxesCustomer::STATE_NOT_APPLICABLE,
+	"other-state" => "Bangkok",
+	"country" => "TH",
+	"zipcode" => "10110",
+	"phone-cc" => "+66",
+	"phone" => "955098991",
+	"lang-pref" => "en",
+];
+
+/** No need to set user id if you're using Laravel, it will automatically get the credential from config/logicboxes.php */
+$customer->setUserId($userId)->setApiKey($apiKey);
+
+$response = $customer->signup($customerDetail)->toArray();
+print_r($response);
---
+++ src/LogicboxesCustomer.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace LaravelLb;
+
+use LaravelLb\LogicBoxes;
+
+class LogicBoxesCustomer extends LogicBoxes {
+
+    const STATE_NOT_APPLICABLE = "Not Applicable";
+
+	public $resource;
+
+    public function __construct()
+    {
+    	$this->resource = "customers";
+    }
+
+    /**
+     * Sign up the new customer
+     * @return LogicBoxesCustomer
+     * http://manage.logicboxes.com/kb/answer/804
+     */
+    public function signUp($variables)
+    {
+    	$method = "signup";
+    	$this->post($this->resource, $method, $variables);
+    	return $this;
+    }
+    
+}
