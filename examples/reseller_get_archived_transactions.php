4415d71bf77dd89f328713c239e10a5d0f4bbd16
Patompong Savaengsuk <pond@netearth.net>
2017-03-01 10:00:55 +0700

    Add methods to get archived reseller and customer transactions.

---
+++ examples/reseller_get_archived_transactions.php
@@ -0,0 +1,21 @@
+<?php
+
+require __DIR__.'/autoload.php';
+
+use LaravelLb\LogicBoxesTransaction;
+use Carbon\Carbon;
+
+// Setup user id and api key
+$userId = getenv('LB_LIVE_AUTH_USERID');
+$apiKey = getenv('LB_LIVE_API_KEY');
+
+$lb = new LogicBoxesTransaction();
+
+/** No need to set user id if you're using Laravel, it will automatically get the credential from config/logicboxes.php */
+$lb->setUserId($userId)->setApiKey($apiKey)->setTestMode(false);
+
+$from = Carbon::create(2016, 10, 1);
+$to = Carbon::create(2016, 10, 2);
+
+$response = $lb->getResellerArchivedTransactions($from, $to);
+print_r($response);
