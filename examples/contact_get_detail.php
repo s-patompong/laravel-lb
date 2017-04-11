704d77b48c2407e3e628640466f8337d2aae0128
Keng <thanan_meepalang@siam.edu>
2017-04-11 12:14:50 +0700

    Re commit

--- examples/contact_get_detail.php
+++ examples/contact_get_detail.php
@@ -4,11 +4,11 @@ require __DIR__.'/autoload.php';
 
 use LaravelLb\LogicBoxesContact;
 
+$contact = new LogicBoxesContact();
+
 // Setup user id and api key
 $userId = getenv('LB_LIVE_AUTH_USERID');
 $apiKey = getenv('LB_LIVE_API_KEY');
-
-$contact = new LogicBoxesContact();
 
 /** No need to set user id if you're using Laravel, it will automatically get the credential from config/logicboxes.php */
 $contact->setTestMode(false)->setUserId($userId)->setApiKey($apiKey);
