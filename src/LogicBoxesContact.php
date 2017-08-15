eb4f3a55fe2d514e365ca27c2a827ed5ed99fd32
Patompong Savaengsuk <pond@netearth.net>
2017-08-15 10:34:43 +0700

    Add new method to create contact.

--- src/LogicBoxesContact.php
+++ src/LogicBoxesContact.php
@@ -21,6 +21,7 @@ class LogicBoxesContact extends LogicBoxes {
      * https://manage.netearthone.com/kb/answer/793
      *
      * @param LogicBoxesContact
+     * @return $this
      */
     public function search($parameters)
     {
@@ -34,6 +35,7 @@ class LogicBoxesContact extends LogicBoxes {
      * https://manage.netearthone.com/kb/answer/792
      *
      * @param LogicBoxesContact
+     * @return $this
      */
     public function details($contactId)
     {
@@ -44,5 +46,20 @@ class LogicBoxesContact extends LogicBoxes {
 
         $response = $this->get($this->resource, $method, $variables);
         return $this;
+    }
+
+    /**
+     * Add a new contact
+     * Ref: https://manage.netearthone.com/kb/answer/790
+     *
+     * @return \LaravelLb\LogicBoxes
+     */
+    public function add($variables)
+    {
+        $method = "add";
+
+        $variables = $this->encodeVariables($variables);
+
+        return $this->post($this->resource, $method, $variables);
     }
 }
